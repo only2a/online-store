3b8ce0c879593adf3185729c05076175407ab454
Dmitry Pilipovich <dmitry.p00.02@mail.ru>
2023-04-14 22:04:14 +0300

    changes

--- server/controllers/ProductController.js
+++ server/controllers/ProductController.js
@@ -1,20 +1,20 @@
-const {Product,Product_info}= require('../models/models');
-const ApiError=require('../error/ApiError');
-const uuid=require('uuid');
-const path= require('path');
+const { Product, Product_info } = require('../models/models');
+const ApiError = require('../error/ApiError');
+const uuid = require('uuid');
+const path = require('path');
 
 
-class ProductController{
-    async create(req, res,next){
-        try{
-            const {name, price, brandId,typeId, info}=req.body;
-            const {img}=req.files;
-            let fileName=uuid.v4() + ".jpeg";
-            img.mv(path.resolve(__dirname,'..', 'static',fileName));
-            const product = await Product.create({name, price, brandId, typeId, img:fileName});
+class ProductController {
+    async create(req, res, next) {
+        try {
+            const { name, price, brandId, typeId, info } = req.body;
+            const { img } = req.files;
+            let fileName = uuid.v4() + ".jpeg";
+            img.mv(path.resolve(__dirname, '..', 'static', fileName));
+            const product = await Product.create({ name, price, brandId, typeId, img: fileName });
 
-            if(info){
-                info= JSON.parse(info);
+            if (info) {
+                info = JSON.parse(info);
                 info.forEach(el => {
                     Product_info.create({
                         title: el.title,
@@ -25,37 +25,37 @@ class ProductController{
             }
             return res.json(product);
         }
-        catch(e){
+        catch (e) {
             console.log(e);
             return next(ApiError.badRequest(e.message));
         }
-        
+
     }
-    async getAll(req, res){
-        let {brandId,typeId,limit,page} = req.query;
+    async getAll(req, res) {
+        let { brandId, typeId, limit, page } = req.query;
         page = page || 1;
-        limit= limit || 9;
-        let offset= page* limit - limit
+        limit = limit || 9;
+        let offset = page * limit - limit
         let products;
-        if( !brandId && !typeId){
-            products= await Product.findAndCountAll({limit, offset});
+        if (!brandId && !typeId) {
+            products = await Product.findAndCountAll({ limit, offset });
         }
-        else if(brandId && !typeId){
-            products= await Product.findAndCountAll({where:{brandId},limit, offset});
+        else if (brandId && !typeId) {
+            products = await Product.findAndCountAll({ where: { brandId }, limit, offset });
         }
-        else if(!brandId && typeId){
-            products= await Product.findAndCountAll({where:{typeId},limit, offset});
+        else if (!brandId && typeId) {
+            products = await Product.findAndCountAll({ where: { typeId }, limit, offset });
         }
-        else if(brandId && typeId){
-            products= await Product.findAndCountAll({where:{brandId, typeId},limit, offset});
+        else if (brandId && typeId) {
+            products = await Product.findAndCountAll({ where: { brandId, typeId }, limit, offset });
         }
         return res.json(products);
     }
-    async getById(req, res){
-        const {id}= req.params;
-        const product= await Product.findOne({where: {id}, include:[{model: Product_info, as: 'info'}]});
+    async getById(req, res) {
+        const { id } = req.params;
+        const product = await Product.findOne({ where: { id }, include: [{ model: Product_info, as: 'info' }] });
         return res.json(product);
     }
 }
 
-module.exports=new ProductController();
+module.exports = new ProductController();
--- server/middleware/productRoleMiddleware.js
+++ server/middleware/productRoleMiddleware.js
@@ -1,32 +1,24 @@
 const jwt = require('jsonwebtoken')
 
-module.exports= function(role){
-    return function(req,res,next){
-        if(req.method === "OPTIONS"){
+module.exports = function (role) {
+    return function (req, res, next) {
+        if (req.method === "OPTIONS") {
             next();
         }
-        try{
+        try {
             const token = req.headers.authorization.split(' ')[1];
-            if(!token){
-                return res.status(401).json({message: "You are not authorized"}); // Bearer asfasfdsf
-            }  
-            const decoded= jwt.verify(token,process.env.SECRET_KEY);
-            if(decoded.role!== role){
-                return res.status(403).json({message:"You don't have access!"});
+            if (!token) {
+                return res.status(401).json({ message: "You are not authorized" }); // Bearer asfasfdsf
+            }
+            const decoded = jwt.verify(token, process.env.SECRET_KEY);
+            if (decoded.role !== role) {
+                return res.status(403).json({ message: "You don't have an access!" });
             }
             req.user = decoded;
             next();
         }
-        catch(e){
-            res.status(403).json({message:"You are not authorized"});
+        catch (e) {
+            res.status(403).json({ message: "You are not authorized" });
         }
     };
 }
-
-
-
-
-
-
-
-
